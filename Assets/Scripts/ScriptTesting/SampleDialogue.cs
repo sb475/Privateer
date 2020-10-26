d0981a19a6fc56b8fafcc0057d34618f69ceae63
tac0cat1 <adam.jh13@gmail.com>
2020-10-25 22:02:06 -0400

    First strike at making basic dialogue boxes and populating with text.
    First strike at making basic dialogue boxes and populating with text.

---
+++ Assets/Scripts/ScriptTesting/SampleDialogue.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class SampleDialogue : MonoBehaviour
+{
+    [TextArea(5, 20)]
+    public string [] conversation;
+    public TextMeshProUGUI conversationText;
+    int conversationIndex = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //HaveConversation();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void NextDialogue()
+    {
+        conversationText.text = conversation[1];
+    }
+
+    public void HaveConversation()
+    {
+        conversationText.text = conversation[conversationIndex];
+        conversationIndex++;
+    }
+}
