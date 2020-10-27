73f3a11137c97cd683e5f2a6333f529266d28059
tac0cat1 <adam.jh13@gmail.com>
2020-10-26 23:24:15 -0400

    minor updates, character and dialogue positioning

--- Assets/Scripts/Control/CharacterInteraction.cs
+++ Assets/Scripts/Control/CharacterInteraction.cs
@@ -15,6 +15,8 @@ namespace RPG.Control
         public Inventory inventory;
         public IDamagable health;
 
+        DialogueSystemTrigger dialogue;
+
 
         public override void Awake()
         {
@@ -24,9 +26,16 @@ namespace RPG.Control
             interactionPoint = gameObject.transform;
             inventory = GetComponent<Inventory>();
             health = GetComponent<IDamagable>();
+            dialogue = GetComponent<DialogueSystemTrigger>();
+
 
             InitializeDefaultBehaviour();
 
+        }
+
+        private void Update()
+        {
+           if (Input.GetKeyDown(KeyCode.T)) dialogue.OnUse();
         }
 
         public override void DefaultInteract(ControllableObject callingController)
